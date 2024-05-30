fdfe4182500a60b8b0e85c568f9f19178667c2b5
isrchary <isrchary@gmail.com>
2024-05-30 08:23:06 +0530

    Files Added

---
+++ StudentManagement/src/com/StudentServlet/DBConnection.java
@@ -0,0 +1,82 @@
+package com.StudentServlet;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
+public class DBConnection {
+	
+	private DataSource dataSource;
+	public DBConnection(DataSource theDataSource) {
+		// TODO Auto-generated constructor stub
+		this.dataSource = theDataSource;
+	}
+	public List<SettersGetters> getStudents() {
+		// TODO Auto-generated method stub
+		List<SettersGetters> Students = new ArrayList<>();
+		
+		Connection myConn = null;
+		Statement myStmt = null;
+		ResultSet myRs = null;
+		
+		try {
+			myConn = dataSource.getConnection();
+			
+			String s = "Select * from Student";
+			
+			myStmt = myConn.createStatement();
+			myRs = myStmt.executeQuery(s);
+			
+			while(myRs.next()) {
+				int ID = myRs.getInt("ID");
+				String NAME = myRs.getString("Student_Name");
+				String GENDER = myRs.getString("Student_Gender");
+				String EMAIL = myRs.getString("Student_Email");
+				String PHONE = myRs.getString("Student_Phone");
+				
+				SettersGetters tempStudent = new SettersGetters(ID, NAME, GENDER, EMAIL, PHONE);
+				
+				Students.add(tempStudent);
+			}
+			
+			System.out.println(Students);
+			return Students;
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			close(myConn,myStmt,myRs);
+		}
+		
+		
+		return null;
+	}
+	
+	
+	
+	private void close(Connection myConn, Statement myStmt, ResultSet myRs) {
+		// TODO Auto-generated method stub
+		try {
+			if(myRs !=null) {
+				myRs.close();
+			}
+			
+			if(myStmt !=null) {
+				myStmt.close();
+			}
+			
+			if(myConn !=null) {
+				myConn.close();
+			}
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
